754d581e226802aa9ebf022f145a96a5f9d62035
Nam Nguyễn <lytunam1999@gmail.com>
2023-05-21 19:55:29 +0700

    added ViewModel for Project WebSale

---
+++ SmartZone/SmartZone/ViewModel/CartItem.cs
@@ -0,0 +1,15 @@
+﻿using SmartZone.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartZone.ViewModel
+{
+    public class CartItem
+    {
+        public Product product { get; set; }
+        public int? Qty { get; set; }
+        public decimal? TotalMoney => Qty * product.SalesPrice.Value;
+    }
+}
